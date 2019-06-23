b29a9e747d0861e9c30b989acff6e371566041ed
Roberth Kulbin <roberth@winter-weht.net>
2019-06-23 22:04:52 +0100

    plugins/credentials/aws: add plugin

---
+++ plugins/credentials/aws/aws.go
@@ -0,0 +1,76 @@
+package aws
+
+import (
+	"context"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/tetratom/cloudsurvey/internal/registry"
+)
+
+func init() {
+	registry.AddCredentials(
+		"aws",
+		func() registry.Credentials {
+			return &AWS{}
+		})
+}
+
+type AWS struct {
+	Profile         string `toml:"profile"`
+	AccessKeyID     string `toml:"access_key_id"`
+	SecretAccessKey string `toml:"secret_access_key"`
+	Token           string `toml:"token"`
+	RoleARN         string `toml:"role_arn"`
+	ExternalID      string `toml:"external_id"`
+	Region          string `toml:"region"`
+	RoleSessionName string `toml:"role_session_name"`
+	SharedConfig    *bool  `toml:"shared_config"`
+
+	session *session.Session
+}
+
+func (plugin *AWS) Init() error {
+	opts := session.Options{}
+
+	if plugin.SharedConfig == nil || *plugin.SharedConfig {
+		opts.SharedConfigState = session.SharedConfigEnable
+	}
+
+	if plugin.Profile != "" {
+		opts.Profile = plugin.Profile
+	} else if plugin.RoleARN != "" {
+		sess := session.Must(session.NewSessionWithOptions(opts))
+		opts.Config.Credentials = stscreds.NewCredentials(
+			sess, plugin.RoleARN, func(provider *stscreds.AssumeRoleProvider) {
+				if plugin.ExternalID != "" {
+					provider.ExternalID = &plugin.ExternalID
+				}
+
+				if plugin.RoleSessionName != "" {
+					provider.RoleSessionName = plugin.RoleSessionName
+				}
+			})
+	} else if plugin.AccessKeyID != "" || plugin.SecretAccessKey != "" || plugin.Token != "" {
+		opts.Config.Credentials = credentials.NewStaticCredentials(
+			plugin.AccessKeyID,
+			plugin.SecretAccessKey,
+			plugin.Token)
+	}
+
+	if plugin.Region != "" {
+		opts.Config.Region = &plugin.Region
+	}
+
+	plugin.session = session.Must(session.NewSessionWithOptions(opts))
+
+	return nil
+}
+
+func (*AWS) Description() string {
+	return "provides pointers to aws-sdk-go sessions"
+}
+
+func (plugin *AWS) Credentials(context.Context) (registry.Credential, error) {
+	return plugin.session, nil
+}
